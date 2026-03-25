2b561cee5d0b075f121d92a551d2336a667952d3
fasluolassery <fasluolassery@gmail.com>
2026-03-25 12:56:15 +0530

    refactor(layout): update RootLayout to fetch dynamic Navbar data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/common/Navbar";
 import NavbarWrapper from "@/components/common/NavbarWrapper";
 import { getNavbar } from "@/lib/queries/navbarData";
 
